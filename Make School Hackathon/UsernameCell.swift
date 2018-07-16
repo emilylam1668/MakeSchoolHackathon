782b6df3d3ebe3e5cdfc578949f96d0ff458f51c
Emily Lam <emilylam1668@users.noreply.github.com>
2018-07-16 16:49:47 -0700

    TableView Added

---
+++ Make School Hackathon/UsernameCell.swift
@@ -0,0 +1,18 @@
+//
+//  UsernameCell.swift
+//  Make School Hackathon
+//
+//  Created by Emily Lam on 7/16/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UsernameCell: UITableViewCell {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var companyTitle: UILabel!
+    
+}
